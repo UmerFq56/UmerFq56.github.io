f67792ce04eabc194fd3872599311b566c063384
Muhammad Umer Farooqui <141590838+UmerFq56@users.noreply.github.com>
2024-08-22 21:01:12 -0400

    g

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 function loadContent(section) {
     const content = document.getElementById('content')
 
+    fetch(`assets/${section}`)
+        .then(response => response.text())
+        .then(html => content.innerHTML = html)
+
     
 }
